91a9372bbc862f24f3a9f2e3703db77469966424
Apprentice <apprentice@dbc25.local>
2013-05-07 16:50:45 -0700

    mvp y'all

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 # See: http://gembundler.com/bundler_setup.html
 #      http://stackoverflow.com/questions/7243486/why-do-you-need-require-bundler-setup
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-
+require_relative 'auth_stuff'
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
 # Require gems we care about
@@ -31,3 +31,5 @@ Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
+
+require 'oauth2'
